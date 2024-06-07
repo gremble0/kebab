e501ebc37560fd0dc4cf98551a1734c72d616c96
gremble0 <hermanstornes@gmail.com>
2024-06-07 21:00:32 +0200

    Evaluate subscriptions and arguments

--- src/runtime/primaries.c
+++ src/runtime/primaries.c
@@ -1,20 +1,21 @@
-#include "runtime/primaries.h"
+#include <stdint.h>
+
 #include "nonstdlib/nerror.h"
 #include "nonstdlib/nlist.h"
 #include "parser/constructors.h"
+#include "parser/expressions.h"
 #include "parser/primaries.h"
 #include "parser/types.h"
 #include "runtime/atoms.h"
 #include "runtime/constructors.h"
 #include "runtime/error.h"
 #include "runtime/expressions.h"
+#include "runtime/primaries.h"
 #include "runtime/runtime.h"
 #include "runtime/scope.h"
 #include "runtime/types.h"
 
-// static rt_value_t *eval_primary_suffix(primary_suffix_t
-
-static rt_value_t *eval_func_call(list_t *arguments, fn_constructor_t *fn,
+static rt_value_t *eval_func_call(list_t *arguments, rt_func_t *fn,
                                   scope_t *scope) {
   scope_t *local_scope = scope_init(scope);
 
@@ -39,6 +40,24 @@ static rt_value_t *eval_func_call(list_t *arguments, fn_constructor_t *fn,
   return v;
 }
 
+static rt_value_t *eval_subscription(expression_t *subscription,
+                                     rt_list_t *list, scope_t *scope) {
+  // TODO: subscription could in the future also be for maps or other structures
+  // so these assumptions may no longer be valid
+
+  rt_value_t *subscription_v = eval_expression(subscription, scope);
+  if (subscription_v->type != type_int)
+    // TODO: make some error for this - can only index lists with ints
+    ASSERT(0);
+
+  if (subscription_v->int_value >= (int64_t)list->size)
+    // TODO: this error is not enough when i implement proper tracing of line
+    // numbers and stuff
+    err_index_out_of_bounds(subscription_v->int_value, list->size);
+
+  return list_get(list, subscription_v->int_value);
+}
+
 rt_value_t *eval_primary(primary_t *prm, scope_t *scope) {
   rt_value_t *v = eval_atom(prm->atom, scope);
 
@@ -47,18 +66,21 @@ rt_value_t *eval_primary(primary_t *prm, scope_t *scope) {
 
   for (size_t i = 0; i < prm->suffixes->size; ++i) {
     primary_suffix_t *psfx = list_get(prm->suffixes, i);
+
+    // TODO: better error handling here
     switch (psfx->type) {
     case PRIMARY_SUBSCRIPTION:
+      if (v->type->type != TYPE_LIST)
+        ASSERT(0);
+
+      v = eval_subscription(psfx->subscription, v->list_value, scope);
     case PRIMARY_ARGUMENT:
+      if (v->type->type != TYPE_FN)
+        ASSERT(0);
+
+      v = eval_func_call(psfx->arguments, v->fn_value, scope);
       break;
     }
-  }
-  if (prm->suffixes->) {
-    // Type error - only functions are callable
-    if (v->type->type != TYPE_FN)
-      ASSERT(0);
-
-    v = eval_func_call(prm->arguments, v->fn_value, scope);
   }
 
   return v;
