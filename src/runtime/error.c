4cc16efbd5315a5b78a95ab2fab5f19b8160140c
gremble0 <hermanstornes@gmail.com>
2024-06-09 22:15:12 +0200

    Clean up runtime errors

--- src/runtime/error.c
+++ src/runtime/error.c
@@ -7,31 +7,30 @@
 #include "utils/utils.h"
 
 _Noreturn void err_type_error(const char *expected, const char *actual) {
+  // Error description
   fprintf(stderr, "type-error: expected type '%s', but got type '%s'\n",
-          expected, actual);
-  exit(1);
-}
-
-_Noreturn void err_list_type_error(const char *expected, const char *actual) {
-  fprintf(stderr,
-          "list-type-error: lists must be homogenous. Declared type '%s' "
-          "actual type '%s'\n",
           expected, actual);
   exit(1);
 }
 
 // TODO: list all defined bindings for a scope?
 _Noreturn void err_name_error(atom_t *name /*, scope_t *scope */) {
-  // TODO: print ^~~~~~ or something based on names span
   span_t span = name->span;
 
-  fprintf(stderr,
-          "%s:%zu:%zu\n"
-          "%s\n" // Should be the line the error comes from
-          // "\n"   // Should be ^~~~~~ under the erroring part of the line
-          "name-error: name '%s' is not defined in the current scope\n",
-          span.file->name, span.start.line, span.start.col,
-          get_line_from_file(span.file->f, span.start.line),
+  // "Coordinates" for where the error comes from
+  fprintf(stderr, "%s:%zu:%zu\n", span.file->name, span.start.line,
+          span.start.col);
+
+  // The line the error comes from
+  fprintf(stderr, "%s", get_line_from_file(span.file->f, span.start.line));
+
+  // A cursor pointing to where in the line the error is `^~~~`
+  fprintf(stderr, "%s^%s\n", repeat_char(' ', span.start.col - 1),
+          repeat_char('~', span.end.col - span.start.col));
+
+  // Error description
+  fprintf(stderr, "name-error: name '%s' is not defined in the current scope\n",
           name->name_value->s);
+
   exit(1);
 }
--- src/runtime/error.h
+++ src/runtime/error.h
@@ -3,5 +3,4 @@
 #include "parser/atoms.h"
 
 _Noreturn void err_type_error(const char *expected, const char *actual);
-_Noreturn void err_list_type_error(const char *expected, const char *actual);
 _Noreturn void err_name_error(atom_t *name);
