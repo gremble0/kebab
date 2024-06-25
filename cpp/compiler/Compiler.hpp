701c889ce66f4051386decd9e6fbf21dbbd2adeb
gremble0 <hermanstornes@gmail.com>
2024-06-25 08:11:48 +0200

    Link with llvm and construct compiler

--- cpp/compiler/Compiler.hpp
+++ cpp/compiler/Compiler.hpp
@@ -1,5 +1,22 @@
-#include "parser/Parser.hpp"
+#include <memory>
 
-namespace Compiler {
-void compile(AstNode root);
-}
+#include "parser/Parser.hpp"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+
+class Compiler {
+private:
+  // TODO: why pointers?
+  std::unique_ptr<llvm::LLVMContext> context;
+  std::unique_ptr<llvm::Module> module;
+  std::unique_ptr<llvm::IRBuilder<>> builder;
+
+public:
+  Compiler()
+      : context(std::make_unique<llvm::LLVMContext>()),
+        module(std::make_unique<llvm::Module>("kebab", *context)),
+        builder(std::make_unique<llvm::IRBuilder<>>(*context)) {}
+
+  void compile(AstNode root);
+};
