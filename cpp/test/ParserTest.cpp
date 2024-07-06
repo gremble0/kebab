c187c699f5f5f850d1ea4391b2fa8202edeb883a
gremble0 <hermanstornes@gmail.com>
2024-07-06 04:57:19 +0200

    Build source dir when building tests

---
+++ cpp/test/ParserTest.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <string>
+
+#include "Files.hpp"
+#include "lexer/Lexer.hpp"
+#include "parser/Parser.hpp"
+#include "gtest/gtest.h"
+
+namespace Kebab {
+namespace Test {
+
+static void parse_file_with_logs(const std::string &basename) {
+  std::string source_path = "parser-source/" + basename + ".keb";
+  std::string log_path = "parser-logs/" + basename + ".log";
+  std::string expected_path = "parser-expected/" + basename + ".log";
+
+  ASSERT_NO_FATAL_FAILURE({ Lexer l(source_path); });
+
+  {
+    std::ofstream log_file(log_path);
+    Lexer l(source_path);
+    // Parser::parse(l);
+  }
+
+  std::ifstream expected_file(expected_path);
+  std::ifstream log_file(log_path);
+  ASSERT_FILES_EQ(expected_file, log_file);
+}
+
+} // namespace Test
+} // namespace Kebab
