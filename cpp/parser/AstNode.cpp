1880128c6b662078f4fcc710c6bdaa03edf45e38
gremble0 <hermanstornes@gmail.com>
2024-07-09 10:19:19 +0200

    Move some implementation from header to source

---
+++ cpp/parser/AstNode.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+#include "logging/Logger.hpp"
+#include "parser/AstNode.hpp"
+
+namespace Kebab {
+namespace Parser {
+
+std::string AstNode::getnline(const std::string &path, size_t line_number) {
+  std::ifstream stream(path);
+  std::string out;
+
+  while (line_number > 0) {
+    --line_number;
+    std::getline(stream, out);
+    if (stream.eof() || stream.fail())
+      return "<reached unexpected end of file>";
+  }
+
+  // unnecessary?
+  stream.close();
+
+  return out;
+}
+
+[[noreturn]] void AstNode::parser_error(const std::string &message, Lexer &lexer) {
+  std::string pretty_position = lexer.pretty_position();
+  std::string labeled_message = "parser-error: " + message;
+
+  std::cerr << pretty_position << labeled_message << std::endl;
+
+  exit(1);
+}
+
+[[noreturn]] void AstNode::compiler_error(const std::string &message) const {
+  std::string line = this->getnline(this->path, this->span.start.line) + '\n';
+  std::string line_cursor =
+      this->span.start.col > 0 ? std::string(this->span.start.col - 1, ' ') + "^\n" : "^\n";
+  std::string labeled_message = "compiler-error: " + message;
+
+  std::cerr << line << line_cursor << labeled_message << std::endl;
+
+  exit(1);
+}
+
+void AstNode::start_parsing(Lexer &lexer, const std::string &node_name) {
+  // maybe some #ifdef for logging (this would affect testing too)
+  Logger::log_with_indent(node_name);
+  this->span.start = lexer.position();
+  this->path = lexer.path;
+}
+
+void AstNode::finish_parsing(Lexer &lexer, const std::string &node_name) {
+  // maybe some #ifdef for logging (this would affect testing too)
+  Logger::log_with_dedent(node_name);
+  this->span.end = lexer.position();
+}
+
+void AstNode::expect(Lexer &lexer, Token::Type type) {
+  if (lexer.cur_token->type != type)
+    parser_error("unexpected token '" + lexer.cur_token->to_string_short() + "' expected: '" +
+                     Token::type_to_string(type) + '\'',
+                 lexer);
+}
+
+void AstNode::expect(Lexer &lexer, Token::Type either, Token::Type or_) {
+  if (lexer.cur_token->type != either && lexer.cur_token->type != or_)
+    parser_error("unexpected token '" + lexer.cur_token->to_string_short() + "' expected: '" +
+                     Token::type_to_string(either) + "' or '" + Token::type_to_string(or_) + '\'',
+                 lexer);
+}
+
+void AstNode::skip(Lexer &lexer, Token::Type type) {
+  expect(lexer, type);
+  lexer.advance();
+}
+
+void AstNode::skip(Lexer &lexer, Token::Type either, Token::Type or_) {
+  expect(lexer, either, or_);
+  lexer.advance();
+}
+
+/**
+ * @return whether the specified token was skipped
+ */
+bool AstNode::ignore(Lexer &lexer, Token::Type type) {
+  if (lexer.cur_token->type == type) {
+    lexer.advance();
+    return true;
+  }
+  return false;
+}
+
+} // namespace Parser
+} // namespace Kebab
--- cpp/parser/AstNode.hpp
+++ cpp/parser/AstNode.hpp
@@ -2,13 +2,11 @@
 #define KEBAB_PARSER_HPP
 
 #include <cstdint>
-#include <iostream>
 #include <memory>
 #include <string>
 
 #include "compiler/Compiler.hpp"
 #include "lexer/Lexer.hpp"
-#include "logging/Logger.hpp"
 #include "llvm/IR/Value.h"
 
 namespace Kebab {
@@ -19,22 +17,7 @@ private:
   /**
    * @brief get the nth line from a file given by path
    */
-  static std::string getnline(const std::string &path, size_t line_number) {
-    std::ifstream stream(path);
-    std::string out;
-
-    while (line_number > 0) {
-      --line_number;
-      std::getline(stream, out);
-      if (stream.eof() || stream.fail())
-        return "<reached unexpected end of file>";
-    }
-
-    // unnecessary?
-    stream.close();
-
-    return out;
-  }
+  static std::string getnline(const std::string &path, size_t line_number);
 
   /**
    * @brief skip a token with an inner value and return it
@@ -52,64 +35,19 @@ protected:
 
   AstNode() = default;
 
-  [[noreturn]] static void parser_error(const std::string &message, Lexer &lexer) {
-    std::string pretty_position = lexer.pretty_position();
-    std::string labeled_message = "parser-error: " + message;
+  [[noreturn]] static void parser_error(const std::string &message, Lexer &lexer);
+  [[noreturn]] void compiler_error(const std::string &message) const;
 
-    std::cerr << pretty_position << labeled_message << std::endl;
-
-    exit(1);
-  }
-
-  [[noreturn]] void compiler_error(const std::string &message) const {
-    std::string line = this->getnline(this->path, this->span.start.line);
-    std::string line_cursor =
-        this->span.start.col > 0 ? std::string(this->span.start.col - 1, ' ') + "^\n" : "^\n";
-    std::string labeled_message = "compiler-error: " + message;
-
-    std::cerr << line << line_cursor << labeled_message << std::endl;
-
-    exit(1);
-  }
-
-  void start_parsing(Lexer &lexer, const std::string &node_name) {
-    // maybe some #ifdef for logging (this would affect testing too)
-    Logger::log_with_indent(node_name);
-    this->span.start = lexer.position();
-    this->path = lexer.path;
-  }
-
-  void finish_parsing(Lexer &lexer, const std::string &node_name) {
-    // maybe some #ifdef for logging (this would affect testing too)
-    Logger::log_with_dedent(node_name);
-    this->span.end = lexer.position();
-  }
-
-  static void expect(Lexer &lexer, Token::Type type) {
-    if (lexer.cur_token->type != type)
-      parser_error("unexpected token '" + lexer.cur_token->to_string_short() + "' expected: '" +
-                       Token::type_to_string(type) + '\'',
-                   lexer);
-  }
+  void start_parsing(Lexer &lexer, const std::string &node_name);
+  void finish_parsing(Lexer &lexer, const std::string &node_name);
 
   // varargs?
-  static void expect(Lexer &lexer, Token::Type either, Token::Type or_) {
-    if (lexer.cur_token->type != either && lexer.cur_token->type != or_)
-      parser_error("unexpected token '" + lexer.cur_token->to_string_short() + "' expected: '" +
-                       Token::type_to_string(either) + "' or '" + Token::type_to_string(or_) + '\'',
-                   lexer);
-  }
-
-  // Move to lexer?
-  static void skip(Lexer &lexer, Token::Type type) {
-    expect(lexer, type);
-    lexer.advance();
-  }
-
-  static void skip(Lexer &lexer, Token::Type either, Token::Type or_) {
-    expect(lexer, either, or_);
-    lexer.advance();
-  }
+  // move to lexer?
+  static void expect(Lexer &lexer, Token::Type type);
+  static void expect(Lexer &lexer, Token::Type either, Token::Type or_);
+  static void skip(Lexer &lexer, Token::Type type);
+  static void skip(Lexer &lexer, Token::Type either, Token::Type or_);
+  static bool ignore(Lexer &lexer, Token::Type type);
 
   static int64_t skip_int(Lexer &lexer) {
     return skip_value<int64_t, Token::Type::INT_LITERAL>(lexer);
@@ -129,17 +67,6 @@ protected:
 
   static std::string skip_name(Lexer &lexer) {
     return skip_value<std::string, Token::Type::NAME>(lexer);
-  }
-
-  /**
-   * @return whether the specified token was skipped
-   */
-  static bool ignore(Lexer &lexer, Token::Type type) {
-    if (lexer.cur_token->type == type) {
-      lexer.advance();
-      return true;
-    }
-    return false;
   }
 
 public:
