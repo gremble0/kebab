39f3e3db695ced80b3e4dd024491fb431e0360ca
gremble0 <hermanstornes@gmail.com>
2024-07-20 15:23:25 +0200

    Fix initialization of scopes

--- cpp/compiler/Scope.cpp
+++ cpp/compiler/Scope.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-
 #include "Scope.hpp"
 #include "llvm/IR/Value.h"
 
 llvm::Value *Scope::operator[](const std::string &key) {
-  std::cout << "getting value: " << key << std::endl;
   llvm::Value *value = this->map[key];
-  std::cout << "value: " << value << std::endl;
-  return value;
+  if (value == nullptr && this->parent.has_value())
+    return (*this->parent->get())[key];
+  else
+    return value;
 }
--- cpp/compiler/Scope.hpp
+++ cpp/compiler/Scope.hpp
@@ -1,15 +1,16 @@
 #include <memory>
+#include <optional>
 #include <unordered_map>
 
 #include "llvm/IR/Value.h"
 
 class Scope {
 private:
-  std::shared_ptr<Scope> parent;
+  std::optional<std::shared_ptr<Scope>> parent;
   std::unordered_map<std::string, llvm::Value *> map;
 
 public:
-  Scope();
+  Scope() = default;
   Scope(std::shared_ptr<Scope> parent) : parent(parent) {}
 
   llvm::Value *operator[](const std::string &key);
