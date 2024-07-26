2fca8687b39e59a05c8a5c17657f57b3f7475584
gremble0 <hermanstornes@gmail.com>
2024-07-27 00:11:26 +0200

    Move variable declarations to source file logger

--- compiler/logging/Logger.cpp
+++ compiler/logging/Logger.cpp
@@ -1,6 +1,14 @@
+#include <mutex>
+
 #include "logging/Logger.hpp"
 
 namespace Kebab::Logger {
+
+static inline std::ostream null_stream(nullptr);
+static inline std::ostream *stream = &std::cout;
+// Probably unnecessary but to future proof the logger is made thread safe
+static inline std::mutex mutex;
+static inline size_t indent_depth = 0;
 
 void set_stream(std::ostream &new_stream) {
   std::lock_guard<std::mutex> lock(mutex);
--- compiler/logging/Logger.hpp
+++ compiler/logging/Logger.hpp
@@ -2,16 +2,9 @@
 #define KEBAB_LOGGER_HPP
 
 #include <iostream>
-#include <mutex>
 #include <ostream>
 
 namespace Kebab::Logger {
-
-static inline std::ostream null_stream(0);
-static inline std::ostream *stream = &std::cout;
-// Probably unnecessary but to future proof the logger is made thread safe
-static inline std::mutex mutex;
-static inline size_t indent_depth = 0;
 
 void set_stream(std::ostream &new_stream);
 void silence();
